1f4bb63efc7942490c0089bbc2d25da189171638
Fangjun Kuang <csukuangfj@gmail.com>
2023-08-03 10:08:22 +0800

    fix typos

--- build_darwin_arm64.go
+++ build_darwin_arm64.go
@@ -1,6 +1,6 @@
 //go:build darwin && arm64 && !ios
 
-package sherpa_onnx
+package sherpa_ncnn
 
 // #cgo LDFLAGS: -L ${SRCDIR}/lib/aarch64-apple-darwin -lsherpa-ncnn-c-api -lsherpa-ncnn-core -lkaldi-native-fbank-core -lncnn -Wl,-rpath,${SRCDIR}/lib/aarch64-apple-darwin
 import "C"
